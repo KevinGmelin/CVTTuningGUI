9797c395525dcbbb2410c44466662e0fb4aa8e63
cball <cball@stevens.edu>
2019-09-20 22:26:16 -0400

    Initial Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace RPM_SAE_Project1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button_LogData = new System.Windows.Forms.Button();
+            this.panel_mSpeedGauge = new System.Windows.Forms.Panel();
+            this.panel_mERPMGauge = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button_LogData
+            // 
+            this.button_LogData.Location = new System.Drawing.Point(716, 492);
+            this.button_LogData.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button_LogData.Name = "button_LogData";
+            this.button_LogData.Size = new System.Drawing.Size(181, 116);
+            this.button_LogData.TabIndex = 0;
+            this.button_LogData.Text = "Start Logging Data";
+            this.button_LogData.UseVisualStyleBackColor = true;
+            this.button_LogData.Click += new System.EventHandler(this.Button_LogData_Click);
+            // 
+            // panel_mSpeedGauge
+            // 
+            this.panel_mSpeedGauge.Location = new System.Drawing.Point(490, 30);
+            this.panel_mSpeedGauge.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel_mSpeedGauge.Name = "panel_mSpeedGauge";
+            this.panel_mSpeedGauge.Size = new System.Drawing.Size(414, 408);
+            this.panel_mSpeedGauge.TabIndex = 1;
+            this.panel_mSpeedGauge.Paint += new System.Windows.Forms.PaintEventHandler(this.Panel_mSpeedGauge_Paint);
+            // 
+            // panel_mERPMGauge
+            // 
+            this.panel_mERPMGauge.Location = new System.Drawing.Point(34, 30);
+            this.panel_mERPMGauge.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel_mERPMGauge.Name = "panel_mERPMGauge";
+            this.panel_mERPMGauge.Size = new System.Drawing.Size(414, 408);
+            this.panel_mERPMGauge.TabIndex = 2;
+            this.panel_mERPMGauge.Paint += new System.Windows.Forms.PaintEventHandler(this.Panel_mERPMGauge_Paint);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(941, 652);
+            this.Controls.Add(this.panel_mERPMGauge);
+            this.Controls.Add(this.panel_mSpeedGauge);
+            this.Controls.Add(this.button_LogData);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_LogData;
+        private System.Windows.Forms.Panel panel_mSpeedGauge;
+        private System.Windows.Forms.Panel panel_mERPMGauge;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RPM_SAE_Project1
+{
+    public partial class Form1 : Form
+    {
+        #region Fields & Constants
+        private AGauge mSpeedGauge;
+        private AGauge mERPMGauge;
+
+        private const int BASE_ARC_START_ANGLE_SPEED = 135;
+        private const int BASE_ARC_SWEEP_ANGLE_SPEED = 270;
+        private const uint DEFAULT_MAX_DIAL_SPEED = 500;
+        private bool mbIsIncreasing = true;
+
+        #endregion
+
+        #region Form Initialization & Closing
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeSpeedGauge();
+            InitializeERPMGauge();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Create the gauge and fit it to the backing panel.
+        /// The Y-value is offset 5% to overcome a tendency of the
+        /// gauge to sit a bit higher than expected when centered.
+        /// </summary>
+        private void InitializeSpeedGauge()
+        {
+            mSpeedGauge = new AGauge
+            {
+                MinValue = 0,
+                MaxValue = DEFAULT_MAX_DIAL_SPEED,
+                Parent = panel_mSpeedGauge,
+                Height = panel_mSpeedGauge.Height,
+                Width = panel_mSpeedGauge.Width,
+                BaseArcStart = BASE_ARC_START_ANGLE_SPEED,
+                BaseArcSweep = BASE_ARC_SWEEP_ANGLE_SPEED,
+                Center = new Point(panel_mSpeedGauge.Width / 2, (int)(panel_mSpeedGauge.Height * 0.55))
+            };
+
+            //for (int i = 0; i < mSpeedGauge.RangesEnabled.Length; i++)
+            //{
+            //    mSpeedGauge.RangesEnabled[i] = false;
+            //}
+        }
+
+        /// <summary>
+        /// Create the gauge and fit it to the backing panel.
+        /// The Y-value is offset 5% to overcome a tendency of the
+        /// gauge to sit a bit higher than expected when centered.
+        /// </summary>
+        private void InitializeERPMGauge()
+        {
+            mERPMGauge = new AGauge
+            {
+                MinValue = 0,
+                MaxValue = DEFAULT_MAX_DIAL_SPEED,
+                Parent = panel_mERPMGauge,
+                Height = panel_mERPMGauge.Height,
+                Width = panel_mERPMGauge.Width,
+                BaseArcStart = BASE_ARC_START_ANGLE_SPEED,
+                BaseArcSweep = BASE_ARC_SWEEP_ANGLE_SPEED,
+                Center = new Point(panel_mERPMGauge.Width / 2, (int)(panel_mERPMGauge.Height * 0.55))
+            };
+
+            //for (int i = 0; i < mERPMGauge.RangesEnabled.Length; i++)
+            //{
+            //    mERPMGauge.RangesEnabled[i] = false;
+            //}
+        }
+        private void Button_LogData_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Panel_mERPMGauge_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Panel_mSpeedGauge_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            if (mSpeedGauge.Value == mSpeedGauge.MaxValue)
+            {
+                mbIsIncreasing = false;
+            }
+            else if (mSpeedGauge.Value == 0)
+            {
+                mbIsIncreasing = true;
+            }
+            if (mbIsIncreasing == true)
+            {
+                mSpeedGauge.Value++;
+                mERPMGauge.Value++;
+            }
+            else
+            {
+                mSpeedGauge.Value--;
+                mERPMGauge.Value--;
+            }
+
+
+
+        }
+    }
+}
+
+#endregion
