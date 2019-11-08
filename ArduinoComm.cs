7111e3ec089fd76bc1ce1d531eadc55d25de50b1
cball <cball@stevens.edu>
2019-11-08 16:19:46 -0500

    Branch Creation

--- ArduinoComm.cs
+++ ArduinoComm.cs
@@ -14,6 +14,10 @@ namespace RPM_SAE_Project1
         private Thread mReadThread;
         private long mfEngineCount;
         private long mfWheelCount;
+        private long mfEngineRPM;
+        private long mfWheelRPM;
+        private double mfVelocity;
+        private double mfDiameter = 20.5;
         private readonly Object mDataLock = new Object();
         private long mlTimeStamp;
 
@@ -31,8 +35,9 @@ namespace RPM_SAE_Project1
             mReadThread.Start();
         }
 
-        public long engineCount { //gets engineRPM once engineRPM is unlocked
-            get 
+        public long engineCount
+        { //gets engineRPM once engineRPM is unlocked
+            get
             {
                 lock (mDataLock) //for multiple threads
                 {
@@ -41,12 +46,34 @@ namespace RPM_SAE_Project1
             }
         }
 
-        public long wheelCount { // gets wheelRPM once wheelRPM is unlocked
+        public long engineRPM { //gets engineRPM once engineRPM is unlocked
+            get 
+            {
+                lock (mDataLock) //for multiple threads
+                {
+                    return mfEngineRPM; // count * 60 * 1/250 * 1/nummagnets
+                }
+            }
+        }
+
+
+        public long wheelCount
+        { // gets wheelRPM once wheelRPM is unlocked
             get
             {
                 lock (mDataLock)
                 {
                     return mfWheelCount;
+                }
+            }
+        }
+
+        public long wheelRPM { // gets wheelRPM once wheelRPM is unlocked
+            get
+            {
+                lock (mDataLock)
+                {
+                    return mfWheelRPM; // count * 60 * 1/250 * 1/nummagnets
                 }
             }
         }
@@ -80,6 +107,9 @@ namespace RPM_SAE_Project1
                             mlTimeStamp = long.Parse(data[0]);
                             mfEngineCount = long.Parse(data[1]);
                             mfWheelCount = long.Parse(data[2]);
+                            mfEngineRPM = mfEngineCount * 60 / 250 / 5;
+                            mfWheelRPM = mfWheelCount * 60 / 250 / 6;
+                            mfVelocity = mfWheelRPM * Math.PI * mfDiameter / 1056;
                             
                         }
                         Console.WriteLine(message);
